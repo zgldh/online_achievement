34a0e30183c613b2b7b6d05ca6d646bcfbb79613
zgldh <zgldh@hotmail.com>
2012-09-02 18:04:37 +0800

    buttons bug

--- application/controllers/achievements.php
+++ application/controllers/achievements.php
@@ -8,7 +8,7 @@ class achievements extends MY_Controller
 		$this->setTitle("所有成就--在线成就系统");
 		$this->view('/achievements/all');
 	}
-	
+
 	/**
 	 * 一个成就的详细页面
 	 * @param int $achievement_id
@@ -16,17 +16,17 @@ class achievements extends MY_Controller
 	public function detail($achievement_id)
 	{
 		$this->navbar->setCurrentItem(NavBar::ITEM_NONE);
-		
+
 		$this->loadAchievementModel();
 		$achievement = AchievementPeer::model()->getByPK($achievement_id);
-		
+
 		if(!$achievement)
 		{
 		    show_404();
 		}
-		
+
 		$this->setTitle($achievement->name."--在线成就系统");
-		
+
 		$this->loadIntentModel();
 		$intent = IntentPeer::model()->getByUserAndAchievement($this->webuser->getUserId(), $achievement_id);
 		if($intent)
@@ -36,13 +36,13 @@ class achievements extends MY_Controller
 		}
 		else
 		{
-		    $this->addStyleCode('.procedure-tools{display:none;}');
+		    $this->addStyleCode('.procedures-box ol li .procedure-tools{display:none;}');
 		}
-		
+
 		$data = compact('achievement','intent');
 		$this->view('achievements/detail',$data);
 	}
-	
+
 	/**
 	 * 开始向某成就努力<br />
 	 * 如果条件允许，则添加该intent然后重定向到/detail/$achievement_id<br />
@@ -57,19 +57,19 @@ class achievements extends MY_Controller
         $this->loadIntentModel();
 
         $error = array();
-        
+
         $intent = IntentPeer::model()->getByUserAndAchievement($this->webuser->getUserId(), $achievement_id);
         if(!$intent)
         {
             //TODO 未来加上前提条件过滤
-            
+
             //设立该 intent
             $intent = new IntentPeer();
             $intent->user_id = $this->webuser->getUserId();
             $intent->achievement_id = $achievement_id;
             $intent->save();
         }
-        
+
         if($error)
         {
             //有错误。设置错误信息
